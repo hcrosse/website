65fa7629284f432a4eff0c47a4cf54f1a5ab2b92
Harrison Crosse <harrison@crosse.dev>
2026-02-21 16:34:08 -0500

    use custom remark

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,15 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
+import remarkGlow from './src/plugins/remark-glow.mjs';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://crosse.dev',
   output: 'static',
+  markdown: {
+    remarkPlugins: [remarkGlow],
+  },
   vite: {
     plugins: [tailwindcss()],
   },
