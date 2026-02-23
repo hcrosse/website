35a37b928a21407b7266b717edaef8895868a0d2
Harrison Crosse <harrison@crosse.dev>
2026-02-23 16:28:01 -0500

    Add --remote flag to wrangler r2 fetch in font script

--- scripts/fetch-fonts.sh
+++ scripts/fetch-fonts.sh
@@ -2,5 +2,5 @@
 set -e
 mkdir -p public/fonts
 for f in BerkeleyMono-Regular BerkeleyMono-Bold BerkeleyMono-Oblique BerkeleyMono-Bold-Oblique; do
-  npx wrangler r2 object get "fonts/berkeley_mono_web/${f}.woff2" --file "public/fonts/${f}.woff2"
+  npx wrangler r2 object get "fonts/berkeley_mono_web/${f}.woff2" --file "public/fonts/${f}.woff2" --remote
 done
